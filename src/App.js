98310825401a74a7804f3ff52dc8447038203372
Joel Frydman <joelfrydman@hotmail.com>
2023-03-05 20:08:21 -0300

    first part end

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Navbar } from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
 
   return (
     <div>
       <Navbar />
       <ItemListContainer greeting={"Proximamente..."}/>
+      
 
     </div>
   );
